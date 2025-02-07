e03a661860f33c79b6cb074c1a310db3cf6c7f8c
Gaouditz <145690838+kreos2801@users.noreply.github.com>
2025-02-07 15:45:08 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,47 +1,47 @@
-# Supprimer les tâches existantes si elles existent
-if (Get-ScheduledTask -TaskName "OuvrirSiteWebAuDemarrage" -ErrorAction SilentlyContinue) {
-    Unregister-ScheduledTask -TaskName "OuvrirSiteWebAuDemarrage" -Confirm:$false
+# Delete existing tasks, if any (You can delete this part)
+if (Get-ScheduledTask -TaskName "LauchWorkOrNot" -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskName "LauchWorkOrNot" -Confirm:$false
 }
 
-# Définir l'action pour exécuter un script PowerShell
+# Define the action to execute a PowerShell script
 $scriptContent = @'
 Add-Type -AssemblyName Microsoft.VisualBasic
 
-$result = [Microsoft.VisualBasic.Interaction]::MsgBox("Voulez-vous bosser ?", "YesNo,Question", "Confirmation")
-$defaultBrowser = (Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice").ProgId
+$result = [Microsoft.VisualBasic.Interaction]::MsgBox("Do you want to work ? ?", "YesNo,Question", "Confirmation")
 
+#You can change here what you want to launch when "yes" is pressed.
 if ($result -eq [Microsoft.VisualBasic.MsgBoxResult]::Yes) {
     	Start-Process "outlook"
     	$teams = Get-ChildItem -Path C:\Users\ -Filter ms-teams.exe -Recurse -Force -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName -First 1
     	if ($teams) {
         	Start-Process $teams
     	} else {
-        	Write-Host "ms-teams.exe n'a pas été trouvé."
+        	Write-Host "ms-teams.exe not found."
     	}
-	Start https://dumas.esiea.fr/cas/login?service=https:%2F%2Fsand.esiea.fr%2Fetudiant
+	Start https://gitlab.com/users/sign_in
 } else {
 	$discord = Get-ChildItem -Path C:\Users\ -Filter Discord.exe -Recurse -Force -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName -First 1
     	if ($discord) {
         	Start-Process $discord
     	} else {
-        	Write-Host "Discord.exe n'a pas été trouvé."
+        	Write-Host "Discord.exe not found."
     	}
-	Start https://dumas.esiea.fr/cas/login?service=https:%2F%2Fsand.esiea.fr%2Fetudiant
+	Start https://www.youtube.com/
 }
 '@
 
-$scriptPath = "C:\Users\gaouditz\Documents\000 Esiea\OuvrirSiteWeb.ps1"
+$scriptPath = "C:\your\path\were\the\scriptcontent\will\be\write"
 $scriptContent | Set-Content -Path $scriptPath -Encoding UTF8
 
-# Définir l'action pour exécuter ce script PowerShell
+# Define the action to execute this PowerShell script
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`"" 
 
-# Définir les déclencheurs
+# Define triggers
 $triggerLogOn = New-ScheduledTaskTrigger -AtLogOn
 
-# Définir le principal et les paramètres
+# Define principal and parameters
 $principal = New-ScheduledTaskPrincipal -UserId "$env:USERNAME" -LogonType Interactive -RunLevel Highest
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
 
-# Créer la tâche planifiée
-Register-ScheduledTask -Action $action -Trigger $triggerLogOn -Principal $principal -Settings $settings -TaskName "OuvrirSiteWebAuDemarrage" -Description "Ouvre le site web à la connexion"
+# Create scheduled task
+Register-ScheduledTask -Action $action -Trigger $triggerLogOn -Principal $principal -Settings $settings -TaskName "LauchWorkOrNot" -Description "Starts work configuration or not "
